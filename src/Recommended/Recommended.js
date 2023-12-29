dfaa9fa1d6e47c860b6ac9ce7098c1e48b460b85
Kishan Kumar <kumarkishan2100@gmail.com>
2023-12-29 11:06:24 +0530

    Responsive

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -7,11 +7,11 @@ const Recommended = ({ handleClick }) => {
       <div>
         <h2 className="recommended-title">Recommended</h2>
         <div className="recommended-flex">
-          <Button onClickHandler={handleClick} value="" title="All Products" />
-          <Button onClickHandler={handleClick} value="Nike" title="Nike" />
-          <Button onClickHandler={handleClick} value="Adidas" title="Adidas" />
-          <Button onClickHandler={handleClick} value="Puma" title="Puma" />
-          <Button onClickHandler={handleClick} value="Vans" title="Vans" />
+          <Button onClickHandler={handleClick} value="" title="All Products" className= "product" />
+          <Button onClickHandler={handleClick} value="Nike" title="Nike" className= "product"/>
+          <Button onClickHandler={handleClick} value="Adidas" title="Adidas" className= "product" />
+          <Button onClickHandler={handleClick} value="Puma" title="Puma" className= "product"/>
+          <Button onClickHandler={handleClick} value="Vans" title="Vans" className= "product" />
         </div>
       </div>
     </>
